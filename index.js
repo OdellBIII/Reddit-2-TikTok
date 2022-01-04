a1965a22d6f9e12f63b889dd5ec36da922fadfcc
Odell Blackmon III <blackmoniii.odell@gmail.com>
2022-01-04 17:12:20 -0500

    Setup a local mongodb instance

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+// Imports necessary for web server
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Imports necessary for database interactions
+const {MongoClient} = require('mongodb');
+
+// Testing whether MongoDB works
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+async function main(){
+
+  const uri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000';
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    await listDatabases(client);
+  } catch(e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
 
 app.get('/', (req, res) => {
 
@@ -9,5 +36,7 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
 
-  console.log('Reddit-2-Tiktok listening at http://localhost:${port}');
+  console.log(`Reddit-2-Tiktok listening at http://localhost:${port}`);
 });
+
+main().catch(console.error);
